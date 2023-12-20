353c8a798ade75da1292ff2932110e4a344812ff
lf2007hf <707908214@qq.com>
2023-12-20 11:59:03 +0800

    去掉LEventDelegateVectorInputBox改为引擎自带的SNumericVectorInputBox

--- Source/LEventDelegate/LEventDelegate.Build.cs
+++ Source/LEventDelegate/LEventDelegate.Build.cs
@@ -16,7 +16,6 @@ public class LEventDelegate : ModuleRules
                 "CoreUObject",
                 "Engine",
                 "Slate",
-                "SlateCore",
 				// ... add other public dependencies that you statically link with here ...
             }
             );
